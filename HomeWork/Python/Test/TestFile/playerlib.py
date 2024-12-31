097e1e2e6e349d1d7523a770d02d1cc721e6fb90
Binhchuoizzz <binhndhe161032@gmail.com>
2024-12-31 15:50:17 +0700

    nothing

---
+++ HomeWork/Python/Test/TestFile/playerlib.py
@@ -0,0 +1,8 @@
+
+class Player:
+    # Biến của lớp. 
+    minAge  = 18
+    maxAge = 50
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
---
+++ HomeWork/Python/Test/TestFile/rectlib.py
@@ -0,0 +1,18 @@
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def getWidth(self):
+        return self.width
+
+    def getHeight(self):
+        return self.height
+
+    def getArea(self):
+        return self.width * self.height
+
+    def getPerimeter(self):
+        return 2 * (self.width + self.height)
+
