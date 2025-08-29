08fb8e978c291e87c117b1b9e426a8c0f79dae69
Binhchuoiz <binhndhe161032@gmail.com>
2025-08-29 18:03:32 +0700

    ok

--- HomeWork/Kỳ 3/IAD591.9/Buổi Học/Buổi 6/Firmware/Firmware.ino
+++ HomeWork/Kỳ 3/IAD591.9/Buổi Học/Buổi 6/Firmware/Firmware.ino
@@ -1,160 +1,84 @@
-
-// Include Libraries
-#include "Arduino.h"
-#include "DHT.h"
-#include "LiquidCrystal_PCF8574.h"
-
-
-// Pin Definitions
-#define DHT_PIN_DATA	0
-
-
-
-// Global variables and defines
-// There are several different versions of the LCD I2C adapter, each might have a different address.
-// Try the given addresses by Un/commenting the following rows until LCD works follow the serial monitor prints. 
-// To find your LCD address go to: http://playground.arduino.cc/Main/I2cScanner and run example.
-#define LCD_ADDRESS 0x3F
-//#define LCD_ADDRESS 0x27
-// Define LCD characteristics
-#define LCD_ROWS 2
-#define LCD_COLUMNS 16
-#define SCROLL_DELAY 150
-#define BACKLIGHT 255
-// object initialization
-DHT dht(DHT_PIN_DATA);
-LiquidCrystal_PCF8574 lcdI2C;
-
-
-// define vars for testing menu
-const int timeout = 10000;       //define timeout of 10 sec
-char menuOption = 0;
-long time0;
-
-// Setup the essentials for your circuit to work. It runs first every time your circuit is powered with electricity.
-void setup() 
-{
-    // Setup Serial which is useful for debugging
-    // Use the Serial Monitor to view printed messages
-    Serial.begin(9600);
-    while (!Serial) ; // wait for serial port to connect. Needed for native USB
-    Serial.println("start");
-    
-    dht.begin();
-    // initialize the lcd
-    lcdI2C.begin(LCD_COLUMNS, LCD_ROWS, LCD_ADDRESS, BACKLIGHT); 
-    menuOption = menu();
-    
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+ESP8266WebServer webServer(80);
+char* ssid = "Tên WiFi";
+char* pass = "Password WiFi";
+//=========Biến chứa mã HTLM Website==//
+const char MainPage[] PROGMEM = R"=====(
+  <!DOCTYPE html> 
+  <html>
+   <head> 
+       <title>HOME PAGE</title> 
+       <style> 
+          body {text-align:center;}
+          h1 {color:#003399;}
+          a {text-decoration: none;color:#FFFFFF;}
+          .bt_on {height:50px; width:100px; margin:10px 0;background-color:#FF6600;border-radius:5px;}
+          .bt_off {height:50px; width:100px; margin:10px 0;background-color:#00FF00;border-radius:5px;}
+       </style>
+       <meta charset="UTF-8">
+   </head>
+   <body> 
+      <h1>ESP8266 Web Server</h1> 
+      <div>Trạng thái trên chân D1: <b>OFF</b></div>
+      <div>
+        <button class="bt_on"><a href="/onD1">ON</a></button>
+        <button class="bt_off"><a href="/offD1">OFF</a></button>
+      </div>
+      <div>Trạng thái trên chân D2: <b>OFF</b></div>
+      <div>
+        <button class="bt_on"><a href="/onD2">ON</a></button>
+        <button class="bt_off"><a href="/offD2">OFF</a></button>
+      </div>
+   </body> 
+  </html>
+)=====";
+//=========================================//
+void setup() {
+  pinMode(D1,OUTPUT);
+  pinMode(D2,OUTPUT);
+  digitalWrite(D1,HIGH);
+  digitalWrite(D2,HIGH);
+  WiFi.begin(ssid,pass);
+  Serial.begin(9600);
+  Serial.print("Connecting");
+  while(WiFi.status()!=WL_CONNECTED){
+    delay(500);
+    Serial.print("...");
+  }
+  Serial.println(WiFi.localIP());
+  webServer.on("/",mainpage);
+  webServer.on("/onD1",on_D1);
+  webServer.on("/offD1",off_D1);
+  webServer.on("/onD2",on_D2);
+  webServer.on("/offD2",off_D2);
+  webServer.begin();
 }
-
-// Main logic of your circuit. It defines the interaction between the components you selected. After setup, it runs over and over again, in an eternal loop.
-void loop() 
-{
-    
-    
-    if(menuOption == '1') {
-    // DHT22/11 Humidity and Temperature Sensor - Test Code
-    // Reading humidity in %
-    float dhtHumidity = dht.readHumidity();
-    // Read temperature in Celsius, for Fahrenheit use .readTempF()
-    float dhtTempC = dht.readTempC();
-    Serial.print(F("Humidity: ")); Serial.print(dhtHumidity); Serial.print(F(" [%]\t"));
-    Serial.print(F("Temp: ")); Serial.print(dhtTempC); Serial.println(F(" [C]"));
-
-    }
-    else if(menuOption == '2') {
-    // LCD 16x2 I2C - Test Code
-    // The LCD Screen will display the text of your choice.
-    lcdI2C.clear();                          // Clear LCD screen.
-    lcdI2C.print("  Circuito.io  ");                   // Print print String to LCD on first line
-    lcdI2C.selectLine(2);                    // Set cursor at the begining of line 2
-    lcdI2C.print("     Rocks!  ");                   // Print print String to LCD on second line
-    delay(1000);
-
-    }
-    
-    if (millis() - time0 > timeout)
-    {
-        menuOption = menu();
-    }
-    
+void loop() {
+  webServer.handleClient();
 }
-
-
-
-// Menu function for selecting the components to be tested
-// Follow serial monitor for instrcutions
-char menu()
-{
-
-    Serial.println(F("\nWhich component would you like to test?"));
-    Serial.println(F("(1) DHT22/11 Humidity and Temperature Sensor"));
-    Serial.println(F("(2) LCD 16x2 I2C"));
-    Serial.println(F("(menu) send anything else or press on board reset button\n"));
-    while (!Serial.available());
-
-    // Read data from serial monitor if received
-    while (Serial.available()) 
-    {
-        char c = Serial.read();
-        if (isAlphaNumeric(c)) 
-        {   
-            
-            if(c == '1') 
-    			Serial.println(F("Now Testing DHT22/11 Humidity and Temperature Sensor"));
-    		else if(c == '2') 
-    			Serial.println(F("Now Testing LCD 16x2 I2C"));
-            else
-            {
-                Serial.println(F("illegal input!"));
-                return 0;
-            }
-            time0 = millis();
-            return c;
-        }
-    }
+//==========Chương trình con=================//
+void mainpage(){
+  String s = FPSTR(MainPage);
+  webServer.send(200,"text/html",s);
 }
-
-/*******************************************************
-
-*    Circuito.io is an automatic generator of schematics and code for off
-*    the shelf hardware combinations.
-
-*    Copyright (C) 2016 Roboplan Technologies Ltd.
-
-*    This program is free software: you can redistribute it and/or modify
-*    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation, either version 3 of the License, or
-*    (at your option) any later version.
-
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU General Public License for more details.
-
-*    You should have received a copy of the GNU General Public License
-*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-*    In addition, and without limitation, to the disclaimers of warranties 
-*    stated above and in the GNU General Public License version 3 (or any 
-*    later version), Roboplan Technologies Ltd. ("Roboplan") offers this 
-*    program subject to the following warranty disclaimers and by using 
-*    this program you acknowledge and agree to the following:
-*    THIS PROGRAM IS PROVIDED ON AN "AS IS" AND "AS AVAILABLE" BASIS, AND 
-*    WITHOUT WARRANTIES OF ANY KIND EITHER EXPRESS OR IMPLIED.  ROBOPLAN 
-*    HEREBY DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, INCLUDING BUT 
-*    NOT LIMITED TO IMPLIED WARRANTIES OF MERCHANTABILITY, TITLE, FITNESS 
-*    FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND THOSE ARISING BY 
-*    STATUTE OR FROM A COURSE OF DEALING OR USAGE OF TRADE. 
-*    YOUR RELIANCE ON, OR USE OF THIS PROGRAM IS AT YOUR SOLE RISK.
-*    ROBOPLAN DOES NOT GUARANTEE THAT THE PROGRAM WILL BE FREE OF, OR NOT 
-*    SUSCEPTIBLE TO, BUGS, SECURITY BREACHES, OR VIRUSES. ROBOPLAN DOES 
-*    NOT WARRANT THAT YOUR USE OF THE PROGRAM, INCLUDING PURSUANT TO 
-*    SCHEMATICS, INSTRUCTIONS OR RECOMMENDATIONS OF ROBOPLAN, WILL BE SAFE 
-*    FOR PERSONAL USE OR FOR PRODUCTION OR COMMERCIAL USE, WILL NOT 
-*    VIOLATE ANY THIRD PARTY RIGHTS, WILL PROVIDE THE INTENDED OR DESIRED
-*    RESULTS, OR OPERATE AS YOU INTENDED OR AS MAY BE INDICATED BY ROBOPLAN. 
-*    YOU HEREBY WAIVE, AGREE NOT TO ASSERT AGAINST, AND RELEASE ROBOPLAN, 
-*    ITS LICENSORS AND AFFILIATES FROM, ANY CLAIMS IN CONNECTION WITH ANY OF 
-*    THE ABOVE. 
-********************************************************/
+void on_D1(){
+  digitalWrite(D1,LOW);
+  String s = FPSTR(MainPage);
+  webServer.send(200,"text/html",s);
+}
+void off_D1(){
+  digitalWrite(D1,HIGH);
+  String s = FPSTR(MainPage);
+  webServer.send(200,"text/html",s);
+}
+void on_D2(){
+  digitalWrite(D2,LOW);
+  String s = FPSTR(MainPage);
+  webServer.send(200,"text/html",s);
+}
+void off_D2(){
+  digitalWrite(D2,HIGH);
+  String s = FPSTR(MainPage);
+  webServer.send(200,"text/html",s);
+}
